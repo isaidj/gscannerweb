173c28470418fc881ab744085d7cfa8825d7d15b
isaidj <isaidj1999@gmail.com>
2023-09-05 15:43:20 -0500

    quitar las credenciales s3

--- components/ButtonDownloadS3File.tsx
+++ components/ButtonDownloadS3File.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { PcIcon } from "@/assets/Icons";
-import downloadUrlFile, { getObject } from "../utils/downloadUrlFile";
+// import downloadUrlFile, { getObject } from "../utils/downloadUrlFile";
 
 export const ButtonDownloadS3File = () => {
   return (
     <div
       className="bg-zinc-900  rounded-lg flex items-center gap-2 p-1 px-2 hover:bg-zinc-800 hover:translate-y-0.5 shadow-xl transition-all duration-300 cursor-pointer"
       onClick={() => {
-        getObject();
+        // getObject();
+        console.log("download");
       }}
     >
       <PcIcon className="w-10   h-10 p-0 m-0" />
--- utils/downloadUrlFile.jsx
+++ utils/downloadUrlFile.jsx
@@ -1,29 +1,28 @@
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+// import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 
-const client = new S3Client({
-  region: "us-east-1",
-  credentials: {
-    accessKeyId: "AKIAQUYEXQYY6GKOAMNG",
-    secretAccessKey: "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",
-  },
-});
+// const client = new S3Client({
+//   region: "us-east-1",
+//   credentials: {
 
-const input = {
-  Bucket: "gscanner",
-  Key: "prueba.png",
-};
-//get object
-export const getObject = async () => {
-  try {
-    const command = new GetObjectCommand(input);
-    const response = await client.send(command);
-    const buffer = await new Response(response.Body).arrayBuffer();
-    const blob = new Blob([buffer]);
-    const url = URL.createObjectURL(blob);
-    return url;
-  } catch (error) {
-    console.log(error);
-  }
-};
+//   },
+// });
 
-export default getObject;
+// const input = {
+//   Bucket: "gscanner",
+//   Key: "prueba.png",
+// };
+// //get object
+// export const getObject = async () => {
+//   try {
+//     const command = new GetObjectCommand(input);
+//     const response = await client.send(command);
+//     const buffer = await new Response(response.Body).arrayBuffer();
+//     const blob = new Blob([buffer]);
+//     const url = URL.createObjectURL(blob);
+//     return url;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export default getObject;
