9ba1dce5442c15c9a796595c4a462523d61ba3a3
isaidj <isaidj1999@gmail.com>
2024-03-01 10:52:24 -0500

    Añadir funcionalidad de descarga de archivos desde S3

--- components/ButtonDownloadS3File.tsx
+++ components/ButtonDownloadS3File.tsx
@@ -1,19 +1,49 @@
 "use client";
 import { PcIcon } from "@/assets/Icons";
-// import downloadUrlFile, { getObject } from "../utils/downloadUrlFile";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const ButtonDownloadS3File = () => {
+  const downloadUrlFile = async (url: string, filename: string) => {
+    const response = await toast.promise(
+      fetch(url)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const blobUrl = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = blobUrl;
+          a.download = filename;
+          a.click();
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        }),
+      {
+        pending: "Downloading...",
+        success: "Downloaded!",
+        error: "Error downloading",
+      }
+    );
+
+    return response;
+  };
   return (
-    <button
-      className="bg-zinc-900  rounded-lg flex items-center gap-2 p-1 px-2 hover:bg-zinc-800 hover:translate-y-0.5 shadow-xl transition-all duration-300 cursor-not-allowed"
-      onClick={() => {
-        // getObject();
-        console.log("download");
-      }}
-    >
-      <PcIcon className="w-10   h-10 p-0 m-0" />
-      Download for PC (Windows)
-    </button>
+    <>
+      <button
+        className="bg-zinc-900  rounded-lg flex items-center gap-2 p-1 px-2 hover:bg-zinc-800 hover:translate-y-0.5 shadow-xl transition-all duration-300 "
+        onClick={() => {
+          // getObject();
+          downloadUrlFile(
+            "https://silvia-tcherassi.s3.amazonaws.com/GScanner_Desktop.exe",
+            "GScanner_Desktop.exe"
+          );
+        }}
+      >
+        <PcIcon className="w-10   h-10 p-0 m-0" />
+        Download for PC (Windows)
+      </button>
+      <ToastContainer style={{ position: "absolute", top: "0", right: "0" }} />
+    </>
   );
 };
 export default ButtonDownloadS3File;
